a31635a648193dfbd8f07395d50f62cf8afdd322
Just-a-developer-abhi <abhi3447141@gmail.com>
2023-04-14 15:56:36 +0530

    home push

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Navbar2 from "./components/Navbar2";
 import './Styles/App.scss';
 import './Styles/Navbar.scss';
 import './Styles/Navbar2.scss';
+import './Styles/Home.scss';
 
 function App() {
   return (
     <>
     <Navbar/>
     <Navbar2/>
+    <Home/>
     </>
    
   );
